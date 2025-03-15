dbe518627466f6a7200f6b78e00e3104daf7dcdd
gsuiz <156917760+gsuiz@users.noreply.github.com>
2025-03-15 19:28:25 -0300

    animação do menu toggle criada e animação de links do menu

--- src/components/MenuToggle.jsx
+++ src/components/MenuToggle.jsx
@@ -1,15 +1,29 @@
-import { NavLink } from "react-router"
-import style from "./NavBar.module.css"
+import { NavLink } from 'react-router'
+import style from './NavBar.module.css'
+import { useState } from 'react'
 
 function MenuToggle() {
+  const [clicked, setClicked] = useState(false)
+
+  const handleClick = () => setClicked((prev) => !prev)
+
   return (
     <>
-      <div className={style.navBar__menuToggle}>
+      <div
+        className={`${style['navBar__menuToggle']} ${
+          clicked ? style['navBar__menuToggle--clicked'] : null
+        }`}
+        onClick={handleClick}
+      >
         <div className={style.menuToggle__line}></div>
         <div className={style.menuToggle__line}></div>
         <div className={style.menuToggle__line}></div>
       </div>
-      <ul className={style.navBar__links}>
+      <ul
+        className={`${style.navBar__links} ${
+          clicked ? style['navBar__links--visible'] : null
+        }`}
+      >
         <li>
           <NavLink to="/">Home</NavLink>
         </li>
