aa3da38ba9b3f88fcd0a5caf0c523b07db897628
gsuiz <156917760+gsuiz@users.noreply.github.com>
2025-03-24 14:47:01 -0300

    lógica de resgate de posts criada

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,19 +1,32 @@
 import InputField from '../../components/InputField'
 import styles from './Home.module.css'
+import { Link } from 'react-router'
+import GreenButton from '../../components/GreenButton'
 
 function Home() {
+  const posts = []
+
   return (
     <div className={styles.home}>
       <h1>Veja os nossos posts mais recentes</h1>
       <form>
-        <InputField 
+        <InputField
           type="text"
           placeholder="Ou busque por tags..."
           name="tag"
         />
         <button className={styles.searchBtn}>Pesquisar</button>
       </form>
-      <div className={styles.home__posts}></div>
+      <div className={styles.home__posts}>
+        {posts && !posts.length ? (
+          <div className={styles.home__noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create">
+              <GreenButton text="Criar primeiro post" />
+            </Link>
+          </div>
+        ) : null}
+      </div>
     </div>
   )
 }
